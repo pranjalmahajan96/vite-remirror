52e9c9ac64e07a1996c88f22bdf4fe3196734554
Pranjal Mahajan <pranjalmahajan96@gmail.com>
2024-09-23 18:27:42 +0530

    using local storage for retrieving data for initial content

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import type { RemirrorJSON } from 'remirror';
 import { OnChangeJSON } from '@remirror/react';
 import { WysiwygEditor } from '@remirror/react-editors/wysiwyg';
@@ -6,22 +6,29 @@ import { WysiwygEditor } from '@remirror/react-editors/wysiwyg';
 const STORAGE_KEY = 'remirror-editor-content';
 
 const App: React.FC = () => {
+  const [initialContent] = useState<RemirrorJSON | undefined>(() => {
+    // Retrieve the JSON from localStorage (or undefined if not found)
+    const content = window.localStorage.getItem(STORAGE_KEY);
+    return content ? JSON.parse(content) : undefined;
+  });
+
   const handleEditorChange = useCallback((json: RemirrorJSON) => {
     // Store the JSON in localstorage
     window.localStorage.setItem(STORAGE_KEY, JSON.stringify(json));
   }, []);
 
-  return <MyEditor onChange={handleEditorChange} />;
+  return <MyEditor onChange={handleEditorChange} initialContent={initialContent} />;
 };
 
 interface MyEditorProps {
   onChange: (json: RemirrorJSON) => void;
+  initialContent?: RemirrorJSON;
 }
 
-const MyEditor: React.FC<MyEditorProps> = ({ onChange }) => {
+const MyEditor: React.FC<MyEditorProps> = ({ onChange, initialContent }) => {
   return (
     <div style={{ padding: 16 }}>
-      <WysiwygEditor placeholder='Enter text...'>
+      <WysiwygEditor placeholder='Enter text...' initialContent={initialContent}>
         <OnChangeJSON onChange={onChange} />
       </WysiwygEditor>
     </div>
